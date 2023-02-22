6ede4f006aac8158ce14659d8e8f9cc5c869267f
MatteoBassanin <matteo.bassanin@gmail.com>
2023-02-22 16:10:27 +0100

    hopefully done with .toFixed(2)

--- js/main.js
+++ js/main.js
@@ -12,12 +12,13 @@ console.log(baseTicketCost);
 
 let newTicketcost;
 
+
 if (eta < 18){
-    newTicketcost = baseTicketCost - (baseTicketCost * 20 / 100);
-    //console.log(roundToTwo(newTicketcost));
+    newTicketcost = baseTicketCost - (baseTicketCost * 20 / 100);   
 }else if (eta > 65){
-    newTicketcost = baseTicketCost - (baseTicketCost * 40 / 100);
-    //console.log(newTicketcost);
+    newTicketcost = baseTicketCost - (baseTicketCost * 40 / 100);   
 }
+
+newTicketcost = newTicketcost.toFixed(2);
 
 document.getElementById("cost_ticket").innerHTML = "Il costo del tuo biglietto è di " + newTicketcost ;
