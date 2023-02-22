3f419a44f1e16b38971bd30508fcffefe1cf9c32
MatteoBassanin <matteo.bassanin@gmail.com>
2023-02-22 15:23:05 +0100

    beginning

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const kiloMetri = prompt("Quanti km devi percorrere?");
+
+console.log(kiloMetri);
+
+const eta = prompt("Quanti anni hai ?")
+
+console.log(eta);
+
+let ticketCost = kiloMetri * 0.21;
+
+console.log(ticketCost);
